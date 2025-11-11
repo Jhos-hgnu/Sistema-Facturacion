485a4b8e169fddba8de95908aa7db6c78c37257e
anyi4 <anyi4@MSI>
2025-11-11 10:08:30 -0600

    marielos

--- src/proyecto_farmacia.java
+++ src/proyecto_farmacia.java
@@ -13,18 +13,10 @@ public class proyecto_farmacia {
    public static void main(String[] args) {
         // TODO code application logic here
 
-       /* VistaInicio vistainicio = new VistaInicio();
+       VistaInicio vistainicio = new VistaInicio();
         vistainicio.setVisible(true);
-        */
-  /* JFrame frame = new JFrame("Auditoría");
-frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-frame.setSize(900, 700);
-frame.setLocationRelativeTo(null);
-PanelReporteCompras panel = new PanelReporteCompras();
+        
    
-frame.setContentPane(panel);
-frame.setVisible(true);
-*/
     }
    
     
